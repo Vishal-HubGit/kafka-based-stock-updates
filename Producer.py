9a303a6be5af1b5d2bfc48c9a2bc67d27348cf99
Vishal-HubGit <91049688+Vishal-HubGit@users.noreply.github.com>
2022-04-25 11:59:18 +0530

    Update Producer.py

--- Producer.py
+++ Producer.py
@@ -21,11 +21,11 @@ class Producer:
     if __name__ == "__main__":
         
         symbol = ['AAPL', 'AMZN', 'FB', 'GOOGL', 'MSFT', 'NFLX', 'TSLA', 'TWTR']
-        token = "pk_3e9ccc33732e44ce83989833101f9ff3"
+        token = "<api-key>" #API-key
         
         for sym in symbol:
             data = IEX(token, sym)
             send_data = data.stock_quote()
             print(send_data)
-            producer.send("first_topic", send_data)
+            producer.send("TOPIC", send_data)
             time.sleep(2)
